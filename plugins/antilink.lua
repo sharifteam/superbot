12cb764dd771d81e1c39a0894a321d6aa79c85d2
sharifteam <parastooalizadeh75@yahoo.com>
2016-02-25 00:08:19 +0330

    Update antilink.lua

--- plugins/antilink.lua
+++ plugins/antilink.lua
@@ -25,6 +25,4 @@ return {patterns = {
 "[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]",
 "[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
 "[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
-"[Hh][Tt][Tt][Pp]://",
-"[Ww][Ww][Ww]:",
 }, run = run}
